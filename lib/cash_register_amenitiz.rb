e654505e9663379050632fcfcc13b35900dde590
muvildan <danielvilches98@gmail.com>
2024-04-27 15:47:24 +0200

    [TDD] Add tests for Product access and tests' format

--- lib/cash_register_amenitiz.rb
+++ lib/cash_register_amenitiz.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'cash_register_amenitiz/version'
+require_relative 'cash_register_amenitiz/cart'
+require_relative 'cash_register_amenitiz/product'
 
 module CashRegisterAmenitiz
   class Error < StandardError; end
