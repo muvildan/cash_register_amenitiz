24bd4dc1d9950a6e4d3964ede7f8cf83558d33a3
muvildan <danielvilches98@gmail.com>
2024-04-27 13:49:21 +0200

    [TDD] Provided base conditions

--- spec/cash_register_amenitiz_spec.rb
+++ spec/cash_register_amenitiz_spec.rb
@@ -1,11 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe CashRegisterAmenitiz do
-  it 'has a version number' do
-    expect(CashRegisterAmenitiz::VERSION).not_to be nil
+  let(:cart) { CashRegisterAmenitiz.new }
+
+  # There should be a method to add products to the cart
+  describe '#add_product' do
+    it 'adds a product to the cart' do
+      cart.add_product('GR1')
+      expect(cart.items).to eq(['GR1'])
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#total_price' do
+    # | GR1,GR1 | 3.11€ |
+    it 'calculates the pricing discount requested by the CEO' do
+      cart.add_product('GR1') # 3.11
+      cart.add_product('GR1') # 3.11
+      # 3.11 + 0 = 3.11
+      expect(cart.total_price).to eq(3.11)
+    end
+    # | SR1,SR1,GR1,SR1 | 16.61€ |
+    it 'calculates the pricing discount requested by the COO' do
+      cart.add_product('SR1') # 5.00
+      cart.add_product('SR1') # 5.00
+      cart.add_product('GR1') # 3.11
+      cart.add_product('SR1') # 5.00
+      # 4.50 + 4.50 + 3.11 + 4.50 = 16.61
+      expect(cart.total_price).to eq(16.61)
+    end
+    # | GR1,CF1,SR1,CF1,CF1 | 30.57€ |
+    it 'calculates the pricing discount requested by the VP of Engineering' do
+      cart.add_product('GR1') # 3.11
+      cart.add_product('CF1') # 11.23
+      cart.add_product('SR1') # 5.00
+      cart.add_product('CF1') # 11.23
+      cart.add_product('CF1') # 11.23
+      # 3.11 + 5.00 + 7.489 * 3 = 30.57
+      expect(cart.total_price).to eq(30.57)
+    end
   end
 end
